beca968ffa671170b56936f1aa15308cd1a806b6
Alon Stern <alon.stern206@gmail.com>
2020-04-13 16:35:24 +0300

    splitted to functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import sys
 
@@ -18,7 +19,12 @@ kernel_size = 20
 
 
 def main():
-    dataset = FunctionIdentificationDataset(sys.argv[1], block_size=1000, padding_size=kernel_size - 1)
+    argument_parser = argparse.ArgumentParser()
+    argument_parser.add_argument("dataset_path", help="Path to the directory with the binaries for the dataset "
+                                                      "(e.g ~/security.ece.cmu.edu/byteweight/elf_32")
+    args = argument_parser.parse_args()
+
+    dataset = FunctionIdentificationDataset(args.dataset_path, block_size=1000, padding_size=kernel_size - 1)
 
     train_size = int(len(dataset) * 0.9)
     test_size = len(dataset) - train_size
@@ -26,74 +32,54 @@ def main():
 
     model = CNNModel(embedding_dim=64, vocab_size=258, hidden_dim=16, tagset_size=2, kernel_size=kernel_size)
 
-    loss_function = nn.NLLLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_model(model, train_dataset)
 
-    best_accuracy = 0
-    best_pr = 0
-    best_recall = 0
-    best_f1 = 0
+    test_model(model, test_dataset)
 
-    train_loader = data.DataLoader(train_dataset, shuffle=True)
+
+def test_model(model, test_dataset):
     test_loader = data.DataLoader(test_dataset)
-
-    current_time = datetime.datetime.now()
-    print("start time", current_time)
-    try:
-        for sample_number, (sample, tags) in tqdm.tqdm(enumerate(train_loader), total=len(train_loader)):
+    model.eval()
+    with torch.no_grad():
+        all_tags = []
+        all_tag_scores = []
+        for sample, tags in tqdm.tqdm(test_loader):
             sample = sample[0]
             tags = tags[0]
-            model.zero_grad()
 
             tag_scores = model(sample)
 
-            loss = loss_function(tag_scores, tags)
-            loss.backward()
-            optimizer.step()
+            all_tags.extend(tags.numpy())
+            all_tag_scores.extend(tag_scores.numpy())
 
-            if sample_number % 10000 == 0:
-                model.eval()
-                # for loader, name in ((test_loader, "test"), (train_loader, "train")):
-                for loader, name in ((test_loader, "test"),):
-                    with torch.no_grad():
-                        all_tags = []
-                        all_tag_scores = []
-                        for sample, tags in tqdm.tqdm(loader):
-                            model.zero_grad()
+        all_tags = numpy.array(all_tags)
+        all_tag_scores = numpy.array(all_tag_scores).argmax(axis=1)
+        accuracy = accuracy_score(all_tags, all_tag_scores)
+        pr = precision_score(all_tags, all_tag_scores)
+        recall = recall_score(all_tags, all_tag_scores)
+        f1 = f1_score(all_tags, all_tag_scores)
 
-                            sample = sample[0]
-                            tags = tags[0]
+        print("accuracy: {}".format(accuracy))
+        print("pr: {}".format(pr))
+        print("recall: {}".format(recall))
+        print("f1: {}".format(f1))
 
-                            tag_scores = model(sample)
 
-                            all_tags.extend(tags.numpy())
-                            all_tag_scores.extend(tag_scores.numpy())
+def train_model(model, train_dataset):
+    loss_function = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    train_loader = data.DataLoader(train_dataset, shuffle=True)
+    model.train()
+    for sample, tags in tqdm.tqdm(train_loader):
+        sample = sample[0]
+        tags = tags[0]
+        model.zero_grad()
 
-                        all_tags = numpy.array(all_tags)
-                        all_tag_scores = numpy.array(all_tag_scores).argmax(axis=1)
-                        accuracy = accuracy_score(all_tags, all_tag_scores)
-                        pr = precision_score(all_tags, all_tag_scores)
-                        recall = recall_score(all_tags, all_tag_scores)
-                        f1 = f1_score(all_tags, all_tag_scores)
-                        if name == "test":
-                            if f1 > best_f1:
-                                best_accuracy = accuracy
-                                best_pr = pr
-                                best_recall = recall
-                                best_f1 = f1
-                        print("name: {}".format(name))
-                        print("accuracy: {}".format(accuracy))
-                        print("pr: {}".format(pr))
-                        print("recall: {}".format(recall))
-                        print("f1: {}".format(f1))
-                model.train()
+        tag_scores = model(sample)
 
-    except KeyboardInterrupt:
-        pass
-    print("best_accuracy: {}".format(best_accuracy))
-    print("best_pr: {}".format(best_pr))
-    print("best_recall: {}".format(best_recall))
-    print("best_f1: {}".format(best_f1))
+        loss = loss_function(tag_scores, tags)
+        loss.backward()
+        optimizer.step()
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -6,22 +6,19 @@ class CNNModel(nn.Module):
     def __init__(self, embedding_dim, kernel_size, hidden_dim, vocab_size, tagset_size):
         super().__init__()
         self._kernel_size = kernel_size
-
-        self.hidden_dim = hidden_dim
-
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-        self.conv = nn.Conv2d(1, hidden_dim, kernel_size=(kernel_size, embedding_dim))
-
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self._hidden_dim = hidden_dim
+        self._word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self._conv = nn.Conv2d(1, hidden_dim, kernel_size=(kernel_size, embedding_dim))
+        self._hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
     def forward(self, sample):
-        embeds = self.word_embeddings(sample)
+        embeds = self._word_embeddings(sample)
+        # Converts the vector to a shape Conv2d can work with
         conv_in = embeds.view(1, 1, len(sample), -1)
-        conv_out = self.conv(conv_in)
+        conv_out = self._conv(conv_in)
         conv_out = F.relu(conv_out)
-        hidden_in = conv_out.view(self.hidden_dim, len(sample) + 1 - self._kernel_size).transpose(0, 1)
-        tag_space = self.hidden2tag(hidden_in)
+        hidden_in = conv_out.view(self._hidden_dim, len(sample) + 1 - self._kernel_size).transpose(0, 1)
+        tag_space = self._hidden2tag(hidden_in)
         tag_scores = F.log_softmax(tag_space, dim=1)
 
         return tag_scores

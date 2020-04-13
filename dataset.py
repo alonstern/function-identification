cd355c34d0255b6bdeb28f546ccae64a65326a37
Alon Stern <alon.stern206@gmail.com>
2020-04-13 11:34:56 +0300

    seperated the model to different file

--- dataset.py
+++ dataset.py
@@ -12,9 +12,8 @@ FILE_END = 257
 
 class FunctionIdentificationDataset(data.Dataset):
     def __init__(self, root_directory, block_size, padding_size):
-        self._padding_size = padding_size
         data, tags = self._preprocess_data(root_directory)
-        self._data_blocks, self._tags_blocks = self._split_to_blocks(data, tags, block_size)
+        self._data_blocks, self._tags_blocks = self._split_to_blocks(data, tags, block_size, padding_size)
 
     def __len__(self):
         return len(self._data_blocks)
@@ -25,9 +24,8 @@ class FunctionIdentificationDataset(data.Dataset):
     def _preprocess_data(self, root_directory):
         files_data = []
         files_tags = []
-        # Iterator for every binary in the dataset
-        binaries_path = glob.glob(os.path.join(root_directory, "*", "binary", "*"))
-        for binary_path in tqdm.tqdm(binaries_path):
+        # Iterates over every binary in the dataset
+        for binary_path in tqdm.tqdm(glob.glob(os.path.join(root_directory, "*", "binary", "*"))):
             with open(binary_path, "rb") as binary_file:
                 binary_elf = ELFFile(binary_file)
 
@@ -46,11 +44,13 @@ class FunctionIdentificationDataset(data.Dataset):
 
     def _generate_tags(self, binary_elf: ELFFile):
         text_section = binary_elf.get_section_by_name(".text")
-        tags = numpy.zeros(text_section.data_size, dtype=int)
 
         # text_section["sh_addr"] is the address of the .text section.
         # We need the addresses of the sumbols to be relative to the .text section so we subtract sh_addr from them.
-        function_addresses = [function_address - text_section["sh_addr"] for function_address in self._get_function_addresses(binary_elf)]
+        function_addresses = [function_address - text_section["sh_addr"] for function_address in
+                              self._get_function_addresses(binary_elf)]
+
+        tags = numpy.zeros(text_section.data_size, dtype=int)
         tags[function_addresses] = 1
         return tags
 
@@ -63,26 +63,24 @@ class FunctionIdentificationDataset(data.Dataset):
         return [symbol["st_value"] for symbol in symbol_table.iter_symbols()
                 if symbol["st_info"]["type"] == "STT_FUNC" and symbol["st_size"] != 0]
 
-    def _split_to_blocks(self, data, tags, block_size):
+    def _split_to_blocks(self, data, tags, block_size, padding_size):
         data_blocks = []
         tags_blocks = []
         for file_data, file_tags in zip(data, tags):
             for start_index in range(0, len(file_data), block_size):
-                data_blocks.append(self._get_padded_data(file_data, start_index, block_size))
+                data_blocks.append(self._get_padded_data(file_data, start_index, block_size, padding_size))
                 tags_blocks.append(file_tags[start_index: start_index + block_size])
 
-        return data_blocks, labels_blocks
+        return data_blocks, tags_blocks
 
-    def _get_padded_data(self, file_data, index, block_size):
-        left_padding_number = int(self._padding_size / 2)
-        right_padding_number = self._padding_size - left_padding_number
+    def _get_padded_data(self, file_data, index, block_size, padding_size):
+        left_padding_number = int(padding_size / 2)
+        right_padding_number = padding_size - left_padding_number
 
+        # If there is data availble before the block we will use it for padding. Otherwise we will use FILE_START.
+        # Same for FILE_END.
         left_padding = numpy.array([FILE_START] * (left_padding_number - index), dtype=int)
         right_padding = numpy.array([FILE_END] * (right_padding_number - max(file_data.size - index - block_size, 0)), dtype=int)
         block = file_data[max(index - left_padding_number, 0): index + block_size + right_padding_number]
 
         return numpy.concatenate([left_padding, block, right_padding])
-
-
-if __name__ == '__main__':
-    dataset = FunctionIdentificationDataset("/home/alon/function-identification-dataset/security.ece.cmu.edu/byteweight/elf_32", 1000, 5)
--- main.py
+++ main.py
@@ -1,48 +1,20 @@
-from __future__ import print_function
-
 import datetime
 import sys
 
 import numpy
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import tqdm
 from sklearn.metrics import precision_score, f1_score, recall_score, accuracy_score
 from torch.utils import data
 
 from dataset import FunctionIdentificationDataset
+from model import CNNModel
 
 torch.manual_seed(1)
 
-
 kernel_size = 20
-
-
-class CNNTagger(nn.Module):
-    def __init__(self, embedding_dim, kernel_size, hidden_dim, vocab_size, tagset_size):
-        super(CNNTagger, self).__init__()
-        self._kernel_size = kernel_size
-
-        self.hidden_dim = hidden_dim
-
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-        self.conv = nn.Conv2d(1, hidden_dim, kernel_size=(kernel_size, embedding_dim))
-
-        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
-
-    def forward(self, sample):
-        embeds = self.word_embeddings(sample)
-        conv_in = embeds.view(1, 1, len(sample), -1)
-        conv_out = self.conv(conv_in)
-        conv_out = F.relu(conv_out)
-        hidden_in = conv_out.view(self.hidden_dim, len(sample) + 1 - self._kernel_size).transpose(0, 1)
-        tag_space = self.hidden2tag(hidden_in)
-        tag_scores = F.log_softmax(tag_space, dim=1)
-
-        return tag_scores
 
 
 def main():
@@ -52,7 +24,7 @@ def main():
     test_size = len(dataset) - train_size
     train_dataset, test_dataset = data.random_split(dataset, [train_size, test_size])
 
-    model = CNNTagger(embedding_dim=64, vocab_size=258, hidden_dim=16,  tagset_size=2, kernel_size=kernel_size)
+    model = CNNModel(embedding_dim=64, vocab_size=258, hidden_dim=16, tagset_size=2, kernel_size=kernel_size)
 
     loss_function = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -86,7 +58,7 @@ def main():
                     with torch.no_grad():
                         all_tags = []
                         all_tag_scores = []
-                        for sample, tags in tqdm.tqdm(loader, total=len(loader)):
+                        for sample, tags in tqdm.tqdm(loader):
                             model.zero_grad()
 
                             sample = sample[0]
